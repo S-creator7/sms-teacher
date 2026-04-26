b82753db03c4f7c3ffef21ffea9786cf6cddeaf4
S-creator7 <info@skyrootmarktech.com>
2026-04-27 01:14:14 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0', 
-    port: 3001, 
+    host: '0.0.0.0',
+    port: 3001,
+    allowedHosts: ['teacher.aaplishala.com']   // ✅ add this
   },
   preview: {
     host: '0.0.0.0',
-    port: 3001
-  }
+    port: 3001,
+    allowedHosts: ['teacher.aaplishala.com']   // ✅ add this
+  }
 })
